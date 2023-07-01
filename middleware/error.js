698da26071313fef32fbd3e0a02d9a429309bb4f
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-07-01 12:43:26 +0100

    🌱setup: project files, folders

---
+++ middleware/error.js
@@ -0,0 +1,34 @@
+// NOTE: custom error handler for express
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+	console.log(err);
+
+	let error = { ...err };
+	error.message = err.message;
+
+	// STUB: Mongoose bad ObjectId
+	if (err.name === "CastError") {
+		const message = `Resource not found with id of ${err.value}`;
+		error = new ErrorResponse(message, 404);
+	}
+
+	// STUB: Mongoose duplicate key
+	if (err.code === 11000) {
+		const message = `Duplicate field value entered`;
+		error = new ErrorResponse(message, 400);
+	}
+
+	// STUB: Mongoose validation error
+	if (err.name === "ValidationError") {
+		const message = Object.values(err.errors).map((val) => val.message);
+		error = new ErrorResponse(message, 400);
+	}
+
+	res.status(error.statusCode || 500).json({
+		success: false,
+		error: error.message || "Server Error",
+	});
+};
+
+module.exports = errorHandler;
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+
+// STUB: load environment variables
+dotenv.config({ path: "./config/config.env" });
+
+// STUB: connect to DB
+connectDB();
+
+// STUB: route files
+// const bootcamps = require("./routes/bootcamps");
+
+// STUB: init express
+const app = express();
+
+// STUB: body parser
+app.use(express.json());
+
+// NOTE: setup middleware
+// STUB: dev logging middleware
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+// STUB: mount routers
+// app.use("/api/v1/bootcamps", bootcamps);
+
+// STUB: custom error handler
+// call this after mounting the routes, else it won't work
+app.use(errorHandler);
+
+// NOTE: use process.env to access environment variables
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+			.bold
+	)
+);
+
+// STUB: handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+
+	// close server & exit process
+	server.close(() => process.exit(1));
+});
