26493eb6a6a8a37ab761fd562549151caa6de0d1
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-07-22 10:04:08 +0100

    🔥feat: added description field to todo model

--- models/Todo.js
+++ models/Todo.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 // STUB: create todo schema
 const TodoSchema = new mongoose.Schema({
 	title: { type: String, required: [true, "Please add a todo title"] },
+	description: { type: String },
 	status: { type: String, enum: ["active", "completed"], default: "active" },
 	user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 	createdAt: { type: Date, default: Date.now },
