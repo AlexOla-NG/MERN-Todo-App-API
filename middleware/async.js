471fcaebf7dc983048de3ce1b7f521dedf766faa
Alx Oweka <alexanderolalekanoweka@gmail.com>
2023-08-29 21:46:12 +0100

    📜documentation: added documentation

--- middleware/async.js
+++ middleware/async.js
@@ -1,3 +1,6 @@
+// STUB: wrapper that resolves all promises in req, res cycle
+// @params: function to wrap
+// @returns: promise that resolves to function
 const asyncHandler = (fn) => (req, res, next) =>
 	Promise.resolve(fn(req, res, next)).catch(next);
 
